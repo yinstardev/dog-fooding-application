9d1cd7c1b1a80b2f5aea7ec4df557ec288e134dd
Yin Yue <prinostaged@gmail.com>
2023-12-21 16:35:36 +0530

    changed-to-online-server

--- src/api/getUserAndToken.ts
+++ src/api/getUserAndToken.ts
@@ -18,6 +18,9 @@ import axios from 'axios';
 //     return { email: '', token: '' };
 //   }
 // };
+
+const be_url = process.env.REACT_APP_BE_URL || '';
+
 const getJwtTokenFromLocalStorage = () => {
   // Retrieve the JWT token from localStorage
   const token = localStorage.getItem('token');
@@ -31,19 +34,19 @@ export const fetchUserAndToken = async () => {
 
     if (!jwtToken) {
       // If no JWT token is available, redirect to the login page
-      window.location.replace('http://localhost:1337/login');
+      window.location.replace(`${be_url}/login`);
       return { email: '', token: '' };
     }
 
     // Use the JWT token to authenticate the request to the /whoami endpoint
-    const userResponse = await axios.get('http://localhost:1337/whoami', {
+    const userResponse = await axios.get(`${be_url}/whoami`, {
       withCredentials: true,
     });
 
     if (userResponse.data && userResponse.data.user && userResponse.data.user.nameID) {
       // Now fetch the different token that you need for global state
       const tokenResponse = await axios.post(
-        'http://localhost:1337/getAuthToken',
+        `${be_url}/getAuthToken`,
         {},
         {
           headers: { 'Content-Type': 'application/json' },
@@ -53,12 +56,12 @@ export const fetchUserAndToken = async () => {
       // Return both the user details and the different token
       return { email: userResponse.data.user.nameID, token: tokenResponse.data };
     } else {
-      window.location.replace('http://localhost:1337/login');
+      window.location.replace(`${be_url}/login`);
       return { email: '', token: '' };
     }
   } catch (error) {
     console.error('Error fetching data:', error);
-    window.location.replace('http://localhost:1337/login');
+    window.location.replace(`${be_url}/login`);
     return { email: '', token: '' };
   }
 };
