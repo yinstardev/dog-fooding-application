776134cffb1120b2ee484c08ee0bb259c7d70ad4
Yin Yue <prinostaged@gmail.com>
2023-12-22 09:32:17 +0530

    changed-to-online-server

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ import { store } from '@app/store/store';
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 
-interface EventTarget {
-  state?: 'activated';
-}
+// interface EventTarget {
+//   state?: 'activated';
+// }
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -23,20 +23,20 @@ root.render(
   </React.StrictMode>,
 );
 
-serviceWorkerRegistration.register({
-  onUpdate: (registration) => {
-    const waitingServiceWorker = registration.waiting;
+// serviceWorkerRegistration.register({
+//   onUpdate: (registration) => {
+//     const waitingServiceWorker = registration.waiting;
 
-    if (waitingServiceWorker) {
-      waitingServiceWorker.addEventListener('statechange', (event) => {
-        if ((event.target as EventTarget).state === 'activated') window.location.reload();
-      });
-      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
-    }
-  },
-}); // app will reload if new version of app is available
+//     if (waitingServiceWorker) {
+//       waitingServiceWorker.addEventListener('statechange', (event) => {
+//         if ((event.target as EventTarget).state === 'activated') window.location.reload();
+//       });
+//       waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+//     }
+//   },
+// }); // app will reload if new version of app is available
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
