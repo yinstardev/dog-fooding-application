3f0ff7f03d2dbfa73d14af172c1b14bde5adca34
Yin Yue <prinostaged@gmail.com>
2023-12-22 17:55:10 +0530

    margin-forfilter-button

--- src/pages/tse-embed/support-central-lib.tsx
+++ src/pages/tse-embed/support-central-lib.tsx
@@ -110,7 +110,7 @@ export function SupportCentral() {
 
   return (
     <BaseRow className="test">
-      <BaseButton type="primary" onClick={() => setIsBasicModalOpen(true)}>
+      <BaseButton style={{ marginBottom: '1em' }} type="primary" onClick={() => setIsBasicModalOpen(true)}>
         <FilterIcon />
       </BaseButton>
       <BaseModal
